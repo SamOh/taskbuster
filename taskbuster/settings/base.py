7d0dfa85b8ef362c854048e234cec1a24ed89fd5
Sammy Oh <soh02@college.harvard.edu>
2016-06-05 22:06:36 -0500

    changed requirement files to allow for deployment (pip uninstall and then pip3 install req). Also created procfile and .env

--- taskbuster/settings/base.py
+++ taskbuster/settings/base.py
@@ -20,14 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 from django.core.exceptions import ImproperlyConfigured
- 
+
+
 def get_env_variable(var_name):
     try:
         return os.environ[var_name]
     except KeyError:
         error_msg = "Set the %s environment variable" % var_name
         raise ImproperlyConfigured(error_msg)
- 
+
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
 
