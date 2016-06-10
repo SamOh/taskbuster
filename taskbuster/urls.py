3b706ae4d8a6abc46cc76b1aa7899e2d071a3c6b
Sammy Oh <soh02@college.harvard.edu>
2016-06-10 14:00:39 -0500

    changed urls.py to see if it would fix error

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -5,13 +5,15 @@ from django.conf.urls.i18n import i18n_patterns
 from .views import home, home_files
 
 urlpatterns = [
+    url(r'^$', home, name='home'),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
         home_files, name='home-files'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^accounts/', include('allauth.urls')),
 ]
 
-urlpatterns += i18n_patterns(
-    url(r'^$', home, name='home'),
-    url(r'^admin/', include(admin.site.urls)),
-)
+# urlpatterns += i18n_patterns(
+#     url(r'^$', home, name='home'),
+#     url(r'^admin/', include(admin.site.urls)),
+# )
