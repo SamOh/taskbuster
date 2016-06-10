897837563ee2e26d50d94c4680cbccf304473dfb
Sammy Oh <soh02@college.harvard.edu>
2016-06-10 13:25:06 -0500

    changed allowed hosts

--- taskbuster/settings/production.py
+++ taskbuster/settings/production.py
@@ -7,7 +7,7 @@ import dj_database_url
 DEBUG = True
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 SITE_ID = 3
-ALLOWED_HOSTS_ = ['taskbustertutsgl.herokuapp.com']
+ALLOWED_HOSTS_ = ['taskbuster-project.herokuapp.com']
 DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
