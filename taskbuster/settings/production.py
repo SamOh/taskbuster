73ce71cb3700f15fb2d3deefbab2d0f7ff9097ab
Sammy Oh <soh02@college.harvard.edu>
2016-06-10 13:03:59 -0500

    deleted old database configs in production.py

--- taskbuster/settings/production.py
+++ taskbuster/settings/production.py
@@ -5,16 +5,5 @@ import dj_database_url
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': get_env_variable('DATABASE_NAME'),
-        'USER': get_env_variable('DATABASE_USER'),
-        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
-        'HOST': '',
-        'PORT': '',
-    }
-}
-
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
