c93a5bd6fc7c20b8619f408692a9d5dfa15366da
Sammy Oh <soh02@college.harvard.edu>
2016-06-10 13:38:21 -0500

    added database settings back to production.py

--- taskbuster/settings/production.py
+++ taskbuster/settings/production.py
@@ -4,11 +4,24 @@ from .base import *
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 SITE_ID = 3
 ALLOWED_HOSTS_ = ['taskbuster-project.herokuapp.com']
-DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=500)
