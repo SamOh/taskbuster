3ef380d3cb85ae4de0a5c05a04afde7806bda0fa
Sammy Oh <soh02@college.harvard.edu>
2016-06-10 13:08:39 -0500

    changed production.py file to match that of sammy's on taskbuster

--- taskbuster/settings/production.py
+++ taskbuster/settings/production.py
@@ -1,9 +1,14 @@
+
 # -*- coding: utf-8 -*-
 from .base import *
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
+SITE_ID = 3
+ALLOWED_HOSTS_ = ['taskbustertutsgl.herokuapp.com']
+DATABASES['default'] = dj_database_url.config(conn_max_age=500)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
