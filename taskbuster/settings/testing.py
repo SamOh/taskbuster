23beaaa201d18eb30b49ef1af50e723e79b689f5
Sammy Oh <soh02@college.harvard.edu>
2016-06-12 14:52:54 -0500

    changed testing.py database configs to be default

--- taskbuster/settings/testing.py
+++ taskbuster/settings/testing.py
@@ -7,9 +7,9 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': get_env_variable('DATABASE_NAME'),
-        'USER': get_env_variable('DATABASE_USER'),
-        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'NAME': 'django_rest_tools_tests', # get_env_variable('DATABASE_NAME'),
+        'USER': 'postgres', # get_env_variable('DATABASE_USER'),
+        'PASSWORD': 'postgres', # get_env_variable('DATABASE_PASSWORD'),
         'HOST': '',
         'PORT': '',
     }
